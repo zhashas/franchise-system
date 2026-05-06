f45aa3670350720e0dffc9e4c8bfa978e97f4024
Jan Rhymeson A. Carbona <jannrhymesoncarbona@email.com>
2026-05-06 21:04:37 +0300

    update src/lib/supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://bviswhyukhfcszvlbsko.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2aXN3aHl1a2hmY3N6dmxic2tvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NDkyNTcsImV4cCI6MjA5MDQyNTI1N30.OgVDqSxELgeEC8jBZFWoT6BiHk5aDxo0xxqvhBOynoY'
+const supabaseUrl = "https://nhqkjqjljcrzvdzrmjqp.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ocWtqcWpsamNyenZkenJtanFwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3ODA4MTA1MCwiZXhwIjoyMDkzNjU3MDUwfQ.HYczOzloA8Y08s3GAoXd6Hb8V_tVevLJ4DRZLOLn7J4";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
